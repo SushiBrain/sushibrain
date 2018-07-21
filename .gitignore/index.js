de6f857018ee57568abe8850e93b88b21fe70933
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-07-21 18:37:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,12 +3,11 @@ const bot = new Discord.Client()
 var prefix = ("+")
 
 bot.on('ready', function () {
-  bot.user.SetActivity("+help")
   console.log("Je suis connecté !")
 })
 
 bot.on('message', msg => {
-  if (msgmsg.content === prefix + 'ping') {
+  if (msg.content === prefix + 'ping') {
     msg.reply('pong !')
   }
 })
